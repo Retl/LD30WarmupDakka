b7dd8a9d35b90df4f0fcf84ee0e33d31ef0ab38d
Retl <carlis.moore@gmail.com>
2014-08-21 15:51:48 -0400

    Project Folder Added
    Added the project folder and the little bit of stuff I did by running
    Unity in Wine.

---
+++ LD Warmup 30/Warmup/Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float horiz = 0;
+    public float vert = 0;
+    public float movespeed = 128;
+
+    public bool abilityOne = false;
+
+    public float shotCooldownTimer = 0.0f;
+    public float shotCooldown = 0.1f;
+
+    public GameObject mainShotStyle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+
+        if (shotCooldownTimer > 0)
+        {
+            shotCooldownTimer -= Time.deltaTime;
+        }
+
+        horiz = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+        abilityOne = Input.GetButton("Fire1");
+
+        gameObject.rigidbody2D.velocity = new Vector2(horiz * movespeed * Time.deltaTime, vert * movespeed * Time.deltaTime);
+
+        if (shotCooldownTimer <= 0 && abilityOne && mainShotStyle != null)
+        {
+            Instantiate(mainShotStyle, gameObject.transform.position, Quaternion.identity);
+            shotCooldownTimer += shotCooldown;
+        }
+	
+	}
+}
---
+++ LD Warmup 30/Warmup/Assets/WaveShot.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveShot : MonoBehaviour {
+
+    public float lifetime = 3.0f;
+    public float hspeed = 12.8f;
+    public float vspeed = 32.0f;
+    public float vamp = 10.0f;
+    public float varc = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	    gameObject.rigidbody2D.velocity = new Vector2(hspeed, System.Convert.ToSingle(System.Math.Sin(varc)) * vspeed * Time.deltaTime);
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        lifetime -= Time.deltaTime;
+        varc += 10 * Time.deltaTime;
+        /*
+        if (varc > 2.0f)
+        {
+            varc -= 2.0f;
+        }
+        */
+
+        if (gameObject.GetComponent<Rigidbody2D>() != null)
+        {
+            gameObject.rigidbody2D.velocity = new Vector2(gameObject.rigidbody2D.velocity.x, System.Convert.ToSingle(System.Math.Sin(varc)) * vspeed * vamp * Time.deltaTime);
+        }
+
+        if (LifetimeExpired())
+        {
+            Destroy(gameObject);
+        }
+	
+	}
+
+    protected bool LifetimeExpired()
+    {
+        return lifetime <= 0;
+    }
+}
